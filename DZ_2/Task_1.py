54baa9c12764ce7989dc3f1988b26fb8c560d9c0
unknown <cross_6@mail.ru>
2023-02-05 11:41:28 +0600

    Added Task_1 in DZ_2

--- DZ_2/Task_1.py
+++ DZ_2/Task_1.py
@@ -5,12 +5,17 @@
 # 5 -> 1 0 1 1 0
 # 2
 # 1 вариант
+
 Kol_monet = int (input('Введите сколько монет должно лежать на столе: '))
 Sum_reshka = 0
 Sum_gerb = 0
 Storoni_monet = []
+Otvet_storona = str('Решка')
 for i in range (Kol_monet):
-    Storoni_monet.append (int(input(f'Какой стороной лежит {i} монетка? 1 - герб, 0 - решка.\nИтак: ')))
+    Storona_monet = int(input(f'Какой стороной лежит {i+1} монетка? 1 - герб, 0 - решка.\nИтак: '))
+    while Storona_monet < 0 or Storona_monet > 1:
+        Storona_monet = int(input(f'Монета {i}, может упасть только 1 или 0, 1 - герб, 0 - решка.\nИтак: '))
+    Storoni_monet.append (Storona_monet)
     
 for i in Storoni_monet:
     if i == 0:
@@ -22,5 +27,10 @@ if Sum_reshka > Sum_gerb:
     Kol_perevernut = Kol_monet - Sum_reshka
 else:
     Kol_perevernut = Kol_monet - Sum_gerb
-print(f'\n{Kol_perevernut}')
-print(f'{Kol_monet} -> {Storoni_monet}\n{Kol_perevernut}')
+    Otvet_storona = str('Герб')
+# print(f'\n{Kol_perevernut}')
+print(f'{Kol_monet} -> ', end='')
+print (*Storoni_monet)
+print (Kol_perevernut)
+print('Пояснение: ')
+print(f'При подкидывании монеты {Kol_monet} раз(а), {Kol_monet - Kol_perevernut} раз(а) выпал(а) {Otvet_storona}\nПоэтому минимальным количеством монет, которые нужно перевернуть, будет {Kol_perevernut}.')
